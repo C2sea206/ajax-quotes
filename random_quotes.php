8ef33b9121e8d9563135c61bfc8dda49b63f2217
C2sea206 <96600494+C2sea206@users.noreply.github.com>
2023-08-06 11:13:22 +0000

    added more quotes

--- random_quotes.php
+++ random_quotes.php
@@ -6,6 +6,9 @@ $quotes = [
     "In three words I can sum up everything I've learned about life: it goes on. - Robert Frost",
     "The only way to do great work is to love what you do. - Steve Jobs",
     "Life is what happens when you're busy making other plans. - John Lennon",
+    "I always tell the truth. Even when I lie. - Scarface",
+    "The Only Thing Standing Between You And Your Goal Is The Bulls**t Story You Keep Telling Yourself As To Why You Can't Achieve It. - Jordan Belfort",
+    "Sell Me This Pen. - Jordan Belfort" 
 ];
 
 // Get a random index
